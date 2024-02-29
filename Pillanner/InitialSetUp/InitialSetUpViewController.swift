0207e098502d07eee05ca8c9fe75f32cc152f3e7
bindunglife <k007gb@naver.com>
2024-02-29 16:05:47 +0900

    초기 세팅화면 pr

--- Pillanner/InitialSetUp/InitialSetUpViewController.swift
+++ Pillanner/InitialSetUp/InitialSetUpViewController.swift
@@ -19,6 +19,23 @@ class InitialSetupViewController: UIViewController, UIPickerViewDelegate, UIPick
     private var selectedPillCount: Int?
     private var alarmIntervalDescriptionLabel: UILabel!
     private var selectedAlarmIntervalLabel: UILabel!
+    
+    
+    private var selectedIntervalLabel: UILabel!
+    private var selectedPillCountLabel: UILabel!
+
+    
+    // "확인" 버튼 추가
+    private var confirmButton: UIButton!
+
+    // 선택 정보 저장을 위한 변수
+    private var selectedTimeForQuestion1: (hour: Int, minute: Int, meridiem: String)?
+    private var selectedIntervalForQuestion2: Int?
+    private var selectedPillCountForQuestion3: Int?
+
+    private var tempSelectedTime: (hour: Int, minute: Int, meridiem: String)?
+    private var tempSelectedInterval: Int?
+    private var tempSelectedPillCount: Int?
 
     
     private var nextButton: UIButton!
@@ -51,6 +68,7 @@ class InitialSetupViewController: UIViewController, UIPickerViewDelegate, UIPick
         super.viewDidLoad()
         setupUI()
         updateUI(for: currentQuestion)
+        setupGestureRecognizers()
     }
     
     func setupUI() {
@@ -62,11 +80,21 @@ class InitialSetupViewController: UIViewController, UIPickerViewDelegate, UIPick
         setupNavigationButtons()
         setupMealTimeLabels()
         setupSelectedCountLabel()
-        // 시간 간격 라벨 설정
+        setupIntervalLabel()
+        setupSelectedIntervalLabel()
+        setupSelectedPillCountLabel()
+        setupConfirmButton()
+        setupGestureRecognizers()
+        
+        // 모든 UI 컴포넌트가 설정된 후 UI를 업데이트
+        updateUI(for: currentQuestion)
+    }
+
+    func setupIntervalLabel() {
         intervalLabel = UILabel()
         intervalLabel.textAlignment = .center
         intervalLabel.font = UIFont.preferredFont(forTextStyle: .title3)
-        intervalLabel.isHidden = true // 초기에는 숨김
+        intervalLabel.isHidden = true
         view.addSubview(intervalLabel)
         
         intervalLabel.snp.makeConstraints { make in
@@ -74,14 +102,134 @@ class InitialSetupViewController: UIViewController, UIPickerViewDelegate, UIPick
             make.centerX.equalTo(view)
             make.height.equalTo(50)
         }
+    }
+
+    func setupSelectedIntervalLabel() {
+        selectedIntervalLabel = UILabel()
+        selectedIntervalLabel.textAlignment = .center
+        selectedIntervalLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        view.addSubview(selectedIntervalLabel)
         
-        // 모든 UI 컴포넌트 초기화 및 레이아웃 설정 후 updateUI 호출
-        updateUI(for: currentQuestion)
-        // 선택한 개수를 표시할 라벨 설정
-        setupSelectedCountLabel()
+        selectedIntervalLabel.snp.makeConstraints { make in
+            make.top.equalTo(medicationReminderLabel.snp.bottom).offset(80)
+            make.centerX.equalToSuperview()
+        }
+    }
+
+    func setupSelectedPillCountLabel() {
+        selectedPillCountLabel = UILabel()
+        selectedPillCountLabel.textAlignment = .center
+        selectedPillCountLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        view.addSubview(selectedPillCountLabel)
         
+        selectedPillCountLabel.snp.makeConstraints { make in
+            make.top.equalTo(medicationReminderLabel.snp.bottom).offset(80)
+            make.centerX.equalToSuperview()
+        }
+    }
+
+    
+    func setupConfirmButton() {
+        confirmButton = UIButton(type: .system)
+        confirmButton.setTitle("확인", for: .normal)
+        confirmButton.titleLabel?.font = FontLiteral.title3(style: .bold) // 폰트 설정
+        confirmButton.setTitleColor(UIColor.pointThemeColor, for: .normal) // 텍스트 컬러 설정
+        confirmButton.backgroundColor = .white // 배경색 설정
+        confirmButton.layer.borderWidth = 1.0 // 테두리 두께 설정
+        confirmButton.layer.borderColor = UIColor.pointThemeColor.cgColor // 테두리 색상 설정
+        confirmButton.layer.cornerRadius = 20 // 모서리 둥글기 설정
+        confirmButton.addTarget(self, action: #selector(confirmButtonTapped), for: .touchUpInside)
+        
+        view.addSubview(confirmButton)
+        
+        confirmButton.snp.makeConstraints { make in
+            make.top.equalTo(pickerContainerView.snp.bottom).offset(2) // 피커뷰 2포인트 아래
+            make.left.right.equalTo(pickerContainerView) // 너비는 피커뷰와 동일하게
+            make.height.equalTo(50) // 높이 설정
+        }
     }
     
+    
+    
+    func setupGestureRecognizers() {
+        // intervalLabel 및 selectedCountLabel에 대한 제스처 인식기 설정
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleLabelTap))
+        intervalLabel.addGestureRecognizer(tapGesture)
+        intervalLabel.isUserInteractionEnabled = true
+        
+        let countLabelTapGesture = UITapGestureRecognizer(target: self, action: #selector(handleLabelTap))
+        selectedCountLabel.addGestureRecognizer(countLabelTapGesture)
+        selectedCountLabel.isUserInteractionEnabled = true
+    }
+
+        
+
+    @objc func handleLabelTap(_ sender: UITapGestureRecognizer) {
+        showPickerView()
+    }
+    
+    @objc func confirmButtonTapped() {
+        switch currentQuestion {
+        case 1:
+            if let selectedTime = tempSelectedTime {
+                // 최종 선택된 시간을 저장
+                selectedTimeForQuestion1 = selectedTime
+                
+                // 선택된 시간을 UI에 표시
+                let timeText = "\(selectedTime.meridiem) \(selectedTime.hour)시 \(selectedTime.minute)분"
+                switch currentSelectedMealType {
+                case .breakfast:
+                    selectedBreakfastTimeLabel.text = timeText
+                case .lunch:
+                    selectedLunchTimeLabel.text = timeText
+                case .dinner:
+                    selectedDinnerTimeLabel.text = timeText
+                default: break
+                }
+            }
+        case 2:
+            if let selectedInterval = tempSelectedInterval {
+                // 최종 선택된 간격을 저장
+                selectedIntervalForQuestion2 = selectedInterval
+                // 선택된 간격을 UI에 표시
+                selectedIntervalLabel.text = "\(selectedInterval)분"
+            }
+        case 3:
+            if let selectedCount = tempSelectedPillCount {
+                // 최종 선택된 알약 개수를 저장
+                selectedPillCountForQuestion3 = selectedCount
+                // 선택된 알약 개수를 UI에 표시
+                selectedPillCountLabel.text = "\(selectedCount)개"
+            }
+        default: break
+        }
+        hidePickerView() // 선택을 확정한 후 피커뷰를 숨깁니다.
+        checkIfSelectionsAreComplete() // 모든 선택이 완료되었는지 확인하고 다음 버튼을 활성화합니다.
+    }
+    
+    
+    func checkIfSelectionsAreComplete() {
+        switch currentQuestion {
+        case 1:
+            // 모든 식사 시간이 선택되었는지 확인
+            let allTimesSelected = selectedBreakfastTimeLabel.text != nil && selectedLunchTimeLabel.text != nil && selectedDinnerTimeLabel.text != nil
+            nextButton.isEnabled = allTimesSelected
+            nextButton.setImage(allTimesSelected ? UIImage(named: "nextButtonSelectedImage") : UIImage(named: "nextButtonImage"), for: .normal)
+        case 2:
+            // 간격이 선택되었는지 확인
+            let intervalSelected = selectedIntervalForQuestion2 != nil
+            nextButton.isEnabled = intervalSelected
+            nextButton.setImage(intervalSelected ? UIImage(named: "nextButtonSelectedImage") : UIImage(named: "nextButtonImage"), for: .normal)
+        case 3:
+            // 알약 개수가 선택되었는지 확인
+            let pillCountSelected = selectedPillCountForQuestion3 != nil
+            nextButton.isEnabled = pillCountSelected
+            nextButton.setImage(pillCountSelected ? UIImage(named: "nextButtonSelectedImage") : UIImage(named: "nextButtonImage"), for: .normal)
+        default:
+            nextButton.isEnabled = false
+        }
+    }
+
     func setupPickerContainerView() {
         pickerContainerView = UIView()
         view.addSubview(pickerContainerView)
@@ -215,30 +363,7 @@ class InitialSetupViewController: UIViewController, UIPickerViewDelegate, UIPick
         }
     }
     
-    func setupAlarmIntervalLabels() {
-        // "식전/식후 알람 간격" 설명 라벨 설정
-        alarmIntervalDescriptionLabel = UILabel()
-        alarmIntervalDescriptionLabel.text = "식전/식후 알람 간격"
-        alarmIntervalDescriptionLabel.font = FontLiteral.title3(style: .regular)
-        alarmIntervalDescriptionLabel.textColor = UIColor.pointThemeColor
-        view.addSubview(alarmIntervalDescriptionLabel)
-        
-        alarmIntervalDescriptionLabel.snp.makeConstraints { make in
-            make.top.equalTo(questionLabel.snp.bottom).offset(80)
-            make.centerX.equalToSuperview()
-        }
-        
-        // 선택한 시간 간격을 표시할 라벨 설정
-        selectedAlarmIntervalLabel = UILabel()
-        selectedAlarmIntervalLabel.font = FontLiteral.title3(style: .regular)
-        selectedAlarmIntervalLabel.textColor = UIColor.pointThemeColor
-        view.addSubview(selectedAlarmIntervalLabel)
-        
-        selectedAlarmIntervalLabel.snp.makeConstraints { make in
-            make.top.equalTo(alarmIntervalDescriptionLabel.snp.bottom).offset(5)
-            make.centerX.equalToSuperview()
-        }
-    }
+    
     
     
     func setupSelectedCountLabel() {
@@ -281,7 +406,11 @@ class InitialSetupViewController: UIViewController, UIPickerViewDelegate, UIPick
         selectedBreakfastTimeLabel.isHidden = true
         selectedLunchTimeLabel.isHidden = true
         selectedDinnerTimeLabel.isHidden = true
-        pickerContainerView.isHidden = true // 피커뷰 컨테이너도 숨깁니다.
+        pickerContainerView.isHidden = true
+        selectedPillCountLabel.isHidden = true
+        selectedIntervalLabel.isHidden = true
+        
+        timePickerView.reloadAllComponents()
         
         switch questionNumber {
         case 1:
@@ -300,15 +429,17 @@ class InitialSetupViewController: UIViewController, UIPickerViewDelegate, UIPick
             medicationReminderLabel.text = "원하는 시간에 맞춰 약 섭취에 대한 알람을 보내드릴게요 :)"
             progressView.setProgress(0.5, animated: true)
             intervalLabel.isHidden = false
+            selectedIntervalLabel.isHidden = false
             showPickerView()
         case 3:
             questionLabel.text = "Q. 등록하고 싶은 알약의 종류는 몇 가지인가요?"
             medicationReminderLabel.text = "알약 종류의 개수에 따라 캘린더 등록을 도와드릴게요 :)"
             progressView.setProgress(0.75, animated: true)
             selectedCountLabel.isHidden = false
+            selectedPillCountLabel.isHidden = false
             showPickerView()
         default:
-            break
+            hidePickerView()
         }
         //        timePickerView.reloadAllComponents()
     }
@@ -351,61 +482,47 @@ class InitialSetupViewController: UIViewController, UIPickerViewDelegate, UIPick
         case 1:
             switch component {
             case 0:
-                return meridiem[row]
+                return row < meridiem.count ? meridiem[row] : nil
             case 1:
-                return "\(hours[row])시"
+                return row < hours.count ? "\(hours[row])시" : nil
             case 2:
-                return "\(minutes[row])분"
+                return row < minutes.count ? "\(minutes[row])분" : nil
             default:
                 return nil
             }
         case 2:
-            return "\(intervalMinutes[row])분"
+            return row < intervalMinutes.count ? "\(intervalMinutes[row])분" : nil
         case 3:
-            return "\(pillCounts[row])개"
+            return row < pillCounts.count ? "\(pillCounts[row])개" : nil
         default:
             return nil
         }
     }
-    
+
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         switch currentQuestion {
         case 1:
-            let selectedHour = hours[timePickerView.selectedRow(inComponent: 1)]
-            let selectedMinute = minutes[timePickerView.selectedRow(inComponent: 2)]
-            let selectedMeridiem = meridiem[timePickerView.selectedRow(inComponent: 0)]
-            let selectedTimeText = "\(selectedMeridiem) \(selectedHour)시 \(selectedMinute)분"
-            
-            switch currentSelectedMealType {
-            case .breakfast:
-                selectedBreakfastTimeLabel.text = selectedTimeText
-            case .lunch:
-                selectedLunchTimeLabel.text = selectedTimeText
-            case .dinner:
-                selectedDinnerTimeLabel.text = selectedTimeText
-            default:
-                break
-            }
-            hidePickerView()
+            let selectedHour = hours[pickerView.selectedRow(inComponent: 1)]
+            let selectedMinute = pickerView.selectedRow(inComponent: 2)
+            let selectedMeridiem = meridiem[pickerView.selectedRow(inComponent: 0)]
+            tempSelectedTime = (hour: selectedHour, minute: selectedMinute, meridiem: selectedMeridiem)
         case 2:
             let selectedInterval = intervalMinutes[row]
-            intervalLabel.text = "\(selectedInterval)분"
-            hidePickerView()
+            tempSelectedInterval = selectedInterval
         case 3:
             let selectedCount = pillCounts[row]
-            selectedPillCount = selectedCount
-            selectedCountLabel.text = "\(selectedCount)개"
-            hidePickerView()
+            tempSelectedPillCount = selectedCount
         default:
             break
         }
     }
+
     
     func showPickerView() {
         pickerContainerView.isHidden = false
         UIView.animate(withDuration: 0.3) {
             self.pickerContainerView.snp.remakeConstraints { make in
-                make.bottom.equalTo(self.view.snp.bottom).offset(-150) // 바닥에서 40포인트 위
+                make.bottom.equalTo(self.view.snp.bottom).offset(-200)
                 make.left.right.equalTo(self.view).inset(20)
                 make.height.equalTo(200)
             }
