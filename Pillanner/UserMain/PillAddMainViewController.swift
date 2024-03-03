ebfdffa6336593506fd6b8494a47ffc0b03c83b2
RACHEL056 <parkmj0506@gmail.com>
2024-03-03 10:32:44 +0900

    xib삭제 후 코드베이스

--- Pillanner/UserMain/PillAddMainViewController.swift
+++ Pillanner/UserMain/PillAddMainViewController.swift
@@ -9,31 +9,6 @@ import UIKit
 import SnapKit
 import SwiftUI
 
-struct PreView: PreviewProvider {
-    static var previews: some View {
-        PillAddMainViewController().toPreview()
-    }
-}
-
-#if DEBUG
-extension UIViewController {
-    private struct Preview: UIViewControllerRepresentable {
-        let viewController: UIViewController
-
-        func makeUIViewController(context: Context) -> UIViewController {
-            return viewController
-        }
-
-        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
-        }
-    }
-
-    func toPreview() -> some View {
-        Preview(viewController: self)
-    }
-}
-#endif
-
 final class PillAddMainViewController: UIViewController {
     private let titleLabel: UILabel = {
         let label = UILabel()
