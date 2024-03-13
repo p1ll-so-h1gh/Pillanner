8fd508aa4de47ab7f093eff0eb614b21864172a3
y0unghyun <dl3rpika@gmail.com>
2024-03-13 22:31:32 +0900

    [chore] code convention application

--- Pillanner/Calendar/CalendarViewController.swift
+++ Pillanner/Calendar/CalendarViewController.swift
@@ -11,9 +11,9 @@ import FSCalendar
 import UserNotifications
 
 class CalendarViewController: UIViewController {
-
+    
     private lazy var gradientLayer = CAGradientLayer.dayBackgroundLayer(view: view)
-
+    
     private var isWeeklyMode: Bool = true
     private let sidePadding: CGFloat = 20
     private let cellHeight: CGFloat = 90
@@ -25,7 +25,7 @@ class CalendarViewController: UIViewController {
     
     // TableViewCell 한 번 탭해서 약을 먹은 상태로 바꾸면 다시 돌아오지 못하도록 만들어야 함
     // 대신 한 번 먹는거 체크할 때 확실하게 할 수 있도록 하는 방법 고안해야 할 듯
-
+    
     var medicationSections: [MedicationSection] = [
         MedicationSection(headerTitle: "오전", medications: [
             Medicine(name: "오메가 3", dosage: "1정", time: "10:30", type: "일반"),
@@ -38,9 +38,9 @@ class CalendarViewController: UIViewController {
     
     private var listOfPills = [Pill]()
     private var categoryOfPills = [PillCategory]()
-
+    
     // MARK: - Properties
-
+    
     private let calendar: FSCalendar = {
         let calendar = FSCalendar(frame: .zero)
         calendar.translatesAutoresizingMaskIntoConstraints = false
@@ -54,7 +54,7 @@ class CalendarViewController: UIViewController {
         calendar.appearance.todayColor = .pointThemeColor2
         return calendar
     }()
-
+    
     private let chevronImage: UIImageView = {
         let imageView = UIImageView(image: UIImage(systemName: "chevron.down"))
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -63,33 +63,33 @@ class CalendarViewController: UIViewController {
         imageView.transform = CGAffineTransform(scaleX: 2.0, y: 2.0)
         return imageView
     }()
-
+    
     private let tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.backgroundColor = .clear
         return tableView
     }()
-
+    
     private let refreshControl: UIRefreshControl = {
         let refreshControl = UIRefreshControl()
         refreshControl.tintColor = .pointThemeColor2
         refreshControl.addTarget(self, action: #selector(handleRefresh(_:)), for: .valueChanged)
         return refreshControl
     }()
-
+    
     private struct Constants {
         static let cellIdentifier = "Cell"
         static let sectionHeaderHeight: CGFloat = 50
     }
-
+    
     // MARK: - Lifecycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         UNUserNotificationCenter.current().delegate = self
-
+        
         // 앱 시작 시 알림 권한 요청
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
             if granted {
@@ -99,12 +99,12 @@ class CalendarViewController: UIViewController {
             }
         }
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         view.layer.addSublayer(gradientLayer)
     }
-
+    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         setupCalendar()
@@ -113,7 +113,7 @@ class CalendarViewController: UIViewController {
         setupSwipeGesture()
         setupRefreshControl()
     }
-
+    
     // MARK: - Setup
     
     // Pill List 만드는 순서 (viewWillAppear 함수 내부에 구현하기)
@@ -208,58 +208,58 @@ class CalendarViewController: UIViewController {
         categoryOfPills.append(pillsListAM)
         categoryOfPills.append(pillsListPM)
     }
-
+    
     private func setupCalendar() {
         view.addSubview(calendar)
         view.addSubview(chevronImage)
         calendar.delegate = self
         calendar.dataSource = self
-
+        
         setupChevronTap()
     }
-
+    
     private func setupTableView() {
         view.addSubview(tableView)
         tableView.delegate = self
         tableView.dataSource = self
-
+        
         tableView.register(CheckPillCell.self, forCellReuseIdentifier: Constants.cellIdentifier)
         tableView.allowsMultipleSelection = true
         tableView.separatorStyle = .none
         tableView.layer.cornerRadius = 20
         tableView.layer.masksToBounds = true
     }
-
+    
     // MARK: - Constraint
-
+    
     private func setupConstraint() {
         calendar.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview().inset(sidePadding)
             $0.height.equalTo(550)
         }
-
+        
         chevronImage.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(calendar.snp.bottom)
         }
-
+        
         tableView.snp.makeConstraints {
             $0.top.equalTo(chevronImage.snp.bottom)
             $0.leading.trailing.equalToSuperview().inset(sidePadding)
             $0.bottom.equalTo(view.safeAreaLayoutGuide).offset(-20)
         }
     }
-
+    
     // MARK: - 모두 선택 완료 시 Animation
-
+    
     private func showFireworkAnimation(at position: CGPoint) {
         let emitter = CAEmitterLayer()
         //emitter.emitterPosition = position
         emitter.emitterPosition = CGPoint(x: position.x, y: view.frame.height)
         emitter.emitterShape = .point
         emitter.emitterSize = CGSize(width: 10, height: 10)
-
+        
         let cell = CAEmitterCell()
         cell.contents = UIImage(named: "firework")?.cgImage
         cell.birthRate = 10
@@ -267,35 +267,35 @@ class CalendarViewController: UIViewController {
         cell.velocity = -250
         cell.scale = 0.05
         cell.scaleRange = 0.4
-
+        
         //cell.emissionRange = CGFloat.pi * 2
         cell.emissionRange = -CGFloat.pi / 6
         cell.emissionLongitude = CGFloat.pi / 2
-
+        
         cell.spin = 0
         cell.spinRange = 10
-
+        
         emitter.emitterCells = [cell]
-
+        
         view.layer.addSublayer(emitter)
-
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
             emitter.removeFromSuperlayer()
         }
     }
-
+    
     // MARK: - 캘린더 스와이프
-
+    
     private func setupSwipeGesture() {
         let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
         swipeUp.direction = .up
         view.addGestureRecognizer(swipeUp)
-
+        
         let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
         swipeDown.direction = .down
         view.addGestureRecognizer(swipeDown)
     }
-
+    
     // 달력 스와이프
     @objc private func handleSwipe(_ swipe: UISwipeGestureRecognizer) {
         if swipe.direction == .up {
@@ -305,10 +305,10 @@ class CalendarViewController: UIViewController {
             calendar.appearance.headerTitleFont = .boldSystemFont(ofSize: 20)
             calendar.setScope(.month, animated: true)
         }
-
+        
         flipChevronImage()
     }
-
+    
     // chevron 탭
     @objc private func handleChevronTap() {
         if isWeeklyMode {
@@ -316,37 +316,37 @@ class CalendarViewController: UIViewController {
         } else {
             calendar.setScope(.week, animated: true)
         }
-
+        
         flipChevronImage()
         isWeeklyMode = !isWeeklyMode
     }
-
+    
     // MARK: - Chevron
-
+    
     private func setupChevronTap() {
         let chevronTapGesture = UITapGestureRecognizer(target: self, action: #selector(handleChevronTap))
         chevronImage.isUserInteractionEnabled = true
         chevronImage.addGestureRecognizer(chevronTapGesture)
     }
-
+    
     // chevron 뒤집는 애니메이션
     private func flipChevronImage() {
         UIView.transition(with: chevronImage, duration: 0.5, options: .transitionFlipFromBottom, animations: {
             self.chevronImage.transform = self.chevronImage.transform.scaledBy(x: 1, y: -1)
         }, completion: nil)
     }
-
+    
     // MARK: - 테이블 뷰 헤더
-
+    
     private func createHeaderView(for section: Int) -> UIView {
         let headerView = UIView()
         headerView.backgroundColor = .clear
-
+        
         let headerTitle = UILabel()
         headerTitle.text = categoryOfPills[section].meridiem
         headerTitle.textColor = .black
         headerTitle.font = FontLiteral.title3(style: .bold)
-
+        
         headerView.addSubview(headerTitle)
         headerTitle.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(20)
@@ -354,13 +354,13 @@ class CalendarViewController: UIViewController {
         }
         return headerView
     }
-
+    
     // MARK: - 새로고침
-
+    
     private func setupRefreshControl() {
         tableView.refreshControl = refreshControl
     }
-
+    
     @objc private func handleRefresh(_ sender: UIRefreshControl) {
         DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             sender.endRefreshing()
@@ -371,84 +371,84 @@ class CalendarViewController: UIViewController {
 // MARK: - Extension
 
 extension CalendarViewController: FSCalendarDelegate, FSCalendarDataSource, UITableViewDataSource, UITableViewDelegate, UNUserNotificationCenterDelegate {
-
+    
     // MARK: FSCalendar Delegate
-
+    
     func calendar(_ calendar: FSCalendar, boundingRectWillChange bounds: CGRect, animated: Bool) {
         calendar.snp.updateConstraints {
             $0.height.equalTo(bounds.height)
         }
-
+        
         UIView.animate(withDuration: 0.5) {
             self.view.layoutIfNeeded()
         }
     }
-
+    
     func calendar(_ calendar: FSCalendar, shouldSelect date: Date, at monthPosition: FSCalendarMonthPosition) -> Bool {
         return false
     }
-
+    
     // MARK: UITableView DataSource
-
+    
     func numberOfSections(in tableView: UITableView) -> Int {
         return categoryOfPills.count
     }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categoryOfPills[section].pills.count
     }
-
+    
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         return createHeaderView(for: section)
     }
-
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return Constants.sectionHeaderHeight
     }
-
+    
     // 셀
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath) as! CheckPillCell
         let pill = categoryOfPills[indexPath.section].pills[indexPath.row]
-
+        
         cell.backgroundColor = .clear
         cell.selectionStyle = .none
         cell.configure(with: pill)
-
+        
         return cell
     }
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return cellHeight
     }
-
+    
     // MARK: - UITableView Delegate
-
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // 모두 선택 시
         if let selectedIndexPaths = tableView.indexPathsForSelectedRows, selectedIndexPaths.count == categoryOfPills.flatMap({ $0.pills }).count {
             showFireworkAnimation(at: tableView.center)
         }
-
+        
         // 알림
         let pill = categoryOfPills[indexPath.section].pills[indexPath.row]
         showNotification(message: "'\(pill.title) - \(pill.dosage)'을 복용하셨습니다.")
     }
-
+    
     // MARK: - UserNotification
-
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         completionHandler([.alert, .sound, .badge])
     }
-
+    
     private func showNotification(message: String) {
         let content = UNMutableNotificationContent()
         //content.title = "알림"
         content.body = message
         content.sound = UNNotificationSound.default
-
+        
         let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
-
+        
         UNUserNotificationCenter.current().add(request) { error in
             if let error = error {
                 print("알림 요청이 실패했습니다. 오류: \(error.localizedDescription)")
--- Pillanner/Calendar/CheckPillCell.swift
+++ Pillanner/Calendar/CheckPillCell.swift
@@ -8,19 +8,19 @@
 import UIKit
 
 class CheckPillCell: UITableViewCell {
-
+    
     private var isSelectedCell: Bool = false
     private var pill: Pill?
-
+    
     // MARK: - Properties
-
+    
     private let pillImage: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         return imageView
     }()
-
+    
     private let nameLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -28,7 +28,7 @@ class CheckPillCell: UITableViewCell {
         label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
         return label
     }()
-
+    
     private let countDosageLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -37,7 +37,7 @@ class CheckPillCell: UITableViewCell {
         label.font = UIFont.systemFont(ofSize: 13)
         return label
     }()
-
+    
     private let dosageLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -45,7 +45,7 @@ class CheckPillCell: UITableViewCell {
         label.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
         return label
     }()
-
+    
     private let takingTimeLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -54,7 +54,7 @@ class CheckPillCell: UITableViewCell {
         label.font = UIFont.systemFont(ofSize: 13)
         return label
     }()
-
+    
     private let timeLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -62,20 +62,20 @@ class CheckPillCell: UITableViewCell {
         label.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
         return label
     }()
-
+    
     private let typeLabelView: UIView = {
         let view = UIView()
         view.layer.cornerRadius = 7
         view.backgroundColor = UIColor.pointThemeColor3
         return view
     }()
-
+    
     private let typeLabel: UILabel = {
         let label = UILabel()
         label.font = FontLiteral.body(style: .bold).withSize(14)
         return label
     }()
-
+    
     private let checkmarkImage: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -84,39 +84,39 @@ class CheckPillCell: UITableViewCell {
         imageView.isHidden = true
         return imageView
     }()
-
+    
     // MARK: - Initialization
-
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         setupUI()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         isSelectedCell = selected
         updateCellSelection()
         updateCheckmark()
     }
-
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         setupUI()
     }
-
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         setupUI()
     }
-
+    
     // MARK: - UI Setup
-
+    
     private func setupUI() {
         textLabel?.textColor = .black
         contentView.layer.borderWidth = 2
         contentView.layer.borderColor = UIColor.pointThemeColor2.cgColor
         contentView.layer.cornerRadius = 10
-
+        
         contentView.addSubview(pillImage)
         contentView.addSubview(nameLabel)
         contentView.addSubview(countDosageLabel)
@@ -126,10 +126,10 @@ class CheckPillCell: UITableViewCell {
         contentView.addSubview(typeLabelView)
         typeLabelView.addSubview(typeLabel)
         contentView.addSubview(checkmarkImage)
-
+        
         setupConstraint()
     }
-
+    
     func configure(with pill: Pill) {
         self.pill = pill
         nameLabel.text = pill.title
@@ -137,61 +137,61 @@ class CheckPillCell: UITableViewCell {
         timeLabel.text = pill.intake[0]
         typeLabel.text = pill.type
     }
-
+    
     // MARK: - Constraint
-
+    
     private func setupConstraint() {
         pillImage.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.leading.equalToSuperview().offset(20)
             $0.width.height.equalTo(30)
         }
-
+        
         nameLabel.snp.makeConstraints {
             $0.centerY.equalToSuperview().offset(-13)
             $0.leading.equalTo(pillImage.snp.trailing).offset(20)
         }
-
+        
         countDosageLabel.snp.makeConstraints {
             $0.centerY.equalToSuperview().offset(13)
             $0.leading.equalTo(pillImage.snp.trailing).offset(20)
         }
-
+        
         dosageLabel.snp.makeConstraints {
             $0.centerY.equalTo(countDosageLabel)
             $0.leading.equalTo(countDosageLabel.snp.trailing).offset(10)
         }
-
+        
         takingTimeLabel.snp.makeConstraints {
             $0.centerY.equalTo(countDosageLabel)
             $0.leading.equalTo(dosageLabel.snp.trailing).offset(15)
         }
-
+        
         timeLabel.snp.makeConstraints {
             $0.centerY.equalTo(countDosageLabel)
             $0.leading.equalTo(takingTimeLabel.snp.trailing).offset(10)
         }
-
+        
         typeLabelView.snp.makeConstraints {
             $0.centerY.equalTo(nameLabel)
             $0.leading.equalTo(nameLabel.snp.trailing).offset(15)
             $0.width.equalTo(50)
             $0.height.equalTo(nameLabel)
         }
-
+        
         typeLabel.snp.makeConstraints {
             $0.center.equalTo(typeLabelView)
         }
-
+        
         checkmarkImage.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.trailing.equalToSuperview().offset(-30)
             $0.width.height.equalTo(20)
         }
     }
-
+    
     // MARK: - 셀 선택
-
+    
     private func updateCellSelection() {
         if isSelectedCell, let medicine = pill {
             contentView.layer.backgroundColor = UIColor.white.cgColor
@@ -199,13 +199,13 @@ class CheckPillCell: UITableViewCell {
                let adjustedImage = originalImage.withSaturation(0.1) {
                 pillImage.image = adjustedImage
             }
-
+            
             // 팝 효과 및 크기 조절
             let shrinkTransform = CGAffineTransform(scaleX: 0.9, y: 0.9)
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.2, options: .curveEaseInOut, animations: {
                 self.contentView.transform = shrinkTransform
                 self.typeLabelView.backgroundColor = .systemGray5
-
+                
             }) { animationCompleted in
                 // 줄어든 상태 유지
                 self.contentView.transform = shrinkTransform
@@ -217,18 +217,18 @@ class CheckPillCell: UITableViewCell {
             contentView.transform = .identity
         }
     }
-
+    
     // 체크표시
     private func updateCheckmark() {
         checkmarkImage.isHidden = !isSelectedCell
     }
-
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 8, left: 20, bottom: 8, right: 20))
         contentView.layer.cornerRadius = 15
     }
-
+    
     // 셀 테두리 안쪽만 터치
     override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
         let hitFrame = bounds.inset(by: UIEdgeInsets(top: 8, left: 20, bottom: 8, right: 20))
@@ -244,11 +244,11 @@ extension UIImage {
         let filter = CIFilter(name: "CIColorControls")
         filter?.setValue(ciImage, forKey: kCIInputImageKey)
         filter?.setValue(saturation, forKey: kCIInputSaturationKey)
-
+        
         guard let outputCIImage = filter?.outputImage else { return nil }
         let context = CIContext(options: nil)
         guard let outputCGImage = context.createCGImage(outputCIImage, from: outputCIImage.extent) else { return nil }
-
+        
         return UIImage(cgImage: outputCGImage)
     }
 }
