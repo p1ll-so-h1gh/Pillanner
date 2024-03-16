6caab05ee89b70aaf55d0e533b728e69b5dbe025
bindunglife <k007gb@naver.com>
2024-03-16 12:15:09 +0900

    개인정보처리방침, 이용약관 부분 뷰 수정 및 내용 수정

--- Pillanner/UserMain/Setting/PrivacyPolicyViewController.swift
+++ Pillanner/UserMain/Setting/PrivacyPolicyViewController.swift
@@ -7,6 +7,7 @@
 
 
 
+
 import UIKit
 
 class PrivacyPolicyViewController: UIViewController {
@@ -29,7 +30,7 @@ class PrivacyPolicyViewController: UIViewController {
         label.textColor = .black // 텍스트 색상 설정
         return label
     }()
-
+    
     private func setupUI() {
         // ScrollView 추가
         let scrollView = UIScrollView()
@@ -40,10 +41,10 @@ class PrivacyPolicyViewController: UIViewController {
         scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
         scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-
+        
         // 개인정보 처리방침 내용 레이블 추가
         scrollView.addSubview(privacyPolicyLabel)
-
+        
         privacyPolicyLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             privacyPolicyLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 20),
@@ -58,43 +59,68 @@ class PrivacyPolicyViewController: UIViewController {
         ])
     }
     
+ 
     private func setupPrivacyPolicy() {
-        // Pillanner 서비스 개인정보 처리방침 내용 가져오기
         let privacyPolicyText = """
-        개인정보 처리방침
+     본 개인정보 처리방침은 Pillanner 앱(이하 '서비스')을 이용하는 사용자(이하 '이용자')의 개인정보 보호를 위해 제공자가 준수해야 하는 사항을 규정합니다.
+    
+    
+    1. 수집하는 개인정보 항목
+       
+       ① 이용자가 서비스를 이용하기 위해 제공한 개인정보: 알람 설정, 약 복용 스케줄, 복용 기록 등
+       ② 자동으로 수집되는 정보: 이용자의 디바이스 정보, 앱 이용 로그 등
+    
+    
+    2. 개인정보의 수집 및 이용목적
 
-        본 개인정보 처리방침은 Pillanner 앱(이하 '서비스')을 이용하는 사용자(이하 '이용자')의 개인정보 보호를 위해 제공자가 준수해야 하는 사항을 규정합니다.
+       ① 서비스 제공: 알람 설정과 알약 복용 기록 관리를 위해 개인정보를 수집하고 이용합니다.
+       ② 개인화 서비스 제공: 이용자의 알약 복용 스케줄 및 기록을 분석하여 개인화된 서비스를 제공합니다.
+    
+    
+    3. 개인정보의 보유 및 이용기간
 
-        1. 수집하는 개인정보 항목
-           - 이용자가 서비스를 이용하기 위해 제공한 개인정보: 알람 설정, 복용 스케줄, 복용 기록 등
-           - 자동으로 수집되는 정보: 이용자의 디바이스 정보, 앱 이용 로그 등
-
-        2. 개인정보의 수집 및 이용목적
-           - 서비스 제공: 알람 설정과 알약 복용 기록 관리를 위해 개인정보를 수집하고 이용합니다.
-           - 개인화 서비스 제공: 이용자의 알약 복용 스케줄 및 기록을 분석하여 개인화된 서비스를 제공합니다.
-
-        3. 개인정보의 보유 및 이용기간
-           - 서비스 이용 종료 시 또는 이용자의 요청에 따라 개인정보를 파기합니다.
-
-        4. 개인정보의 제공 및 위탁
-           - 이용자의 동의를 받은 경우나 법령의 규정에 따라 개인정보를 제3자에게 제공하거나 위탁할 수 있습니다.
-
-        5. 개인정보의 보호조치
-           - 개인정보 보호를 위해 기술적, 관리적, 물리적 보안 조치를 취합니다.
-           - 개인정보 처리와 관련한 접근 권한은 최소한의 직원에게만 제공하고 있습니다.
-
-        6. 이용자의 권리와 행사
-           - 개인정보에 대한 열람, 정정, 삭제, 처리정지 요청을 할 수 있습니다.
-
-        7. 개인정보의 파기절차 및 방법
-           - 서비스 이용 종료 시 또는 이용자의 요청에 따라 파기됩니다.
-
-        8. 개인정보 관련 문의
-           - 개인정보 처리에 대한 문의사항은 서비스 내 고객센터를 통해 문의하실 수 있습니다.
-
-        본 개인정보 처리방침은 법령, 정부지침 또는 서비스의 변경에 따라 변경될 수 있습니다. 변경 시에는 변경 내용을 서비스 내에 공지하겠습니다.
-
-        """
-        privacyPolicyLabel.text = privacyPolicyText
+       ① 서비스 이용 종료 시 또는 이용자의 요청에 따라 개인정보를 파기합니다.
+    
+    
+    4. 개인정보의 제공 및 위탁
+    
+       ① 이용자의 동의를 받은 경우나 법령의 규정에 따라 개인정보를 제3자에게 제공하거나 위탁할 수 있습니다.
+    
+    
+    5. 개인정보의 보호조치
+    
+       ① 개인정보 보호를 위해 기술적, 관리적, 물리적 보안 조치를 취합니다.
+       ② 개인정보 처리와 관련한 접근 권한은 최소한의 직원에게만 제공하고 있습니다.
+    
+    
+    6. 이용자의 권리와 행사
+    
+       ① 개인정보에 대한 열람, 정정, 삭제, 처리정지 요청을 할 수 있습니다.
+    
+    
+    7. 개인정보의 파기절차 및 방법
+    
+       ① 서비스 이용 종료 시 또는 이용자의 요청에 따라 파기됩니다.
+    
+    
+    8. 개인정보 관련 문의
+    
+       ① 개인정보 처리에 대한 문의사항은 서비스 내 고객센터를 통해 문의하실 수 있습니다.
+    
+    
+    본 개인정보 처리방침은 법령, 정부지침 또는 서비스의 변경에 따라 변경될 수 있습니다. 변경 시에는 변경 내용을 서비스 내에 공지하겠습니다.
+    """
+        
+        let attributedString = NSMutableAttributedString(string: privacyPolicyText)
+        let boldFontAttribute = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16.0)]
+        
+        // 각 번호에 해당하는 부분의 범위를 찾아서 볼드 처리
+        let boldStrings = ["본 개인정보 처리방침은 Pillanner 앱(이하 '서비스')을 이용하는 사용자(이하 '이용자')의 개인정보 보호를 위해 제공자가 준수해야 하는 사항을 규정합니다.","1. 수집하는 개인정보 항목", "2. 개인정보의 수집 및 이용목적", "3. 개인정보의 보유 및 이용기간", "4. 개인정보의 제공 및 위탁", "5. 개인정보의 보호조치", "6. 이용자의 권리와 행사", "7. 개인정보의 파기절차 및 방법", "8. 개인정보 관련 문의"]
+        for boldString in boldStrings {
+            let range = (privacyPolicyText as NSString).range(of: boldString)
+            attributedString.addAttributes(boldFontAttribute, range: range)
+        }
+        
+        privacyPolicyLabel.attributedText = attributedString
     }
 }
--- Pillanner/UserMain/Setting/TermsofUseViewController.swift
+++ Pillanner/UserMain/Setting/TermsofUseViewController.swift
@@ -7,74 +7,105 @@
 //
 
 
+
 import UIKit
 
 class TermsofUseViewController: UIViewController {
     
-    private let termsOfServiceTextView: UITextView = {
-        let textView = UITextView()
-        textView.isEditable = false
-        textView.isScrollEnabled = true
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        return textView
-    }()
+override func viewDidLoad() {
+    super.viewDidLoad()
+    self.navigationItem.title = "이용약관"
+    view.backgroundColor = .white
+    self.navigationController?.isNavigationBarHidden = false
+    setupUI()
+    setupTermsOfService()
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.navigationItem.title = "이용약관"
-        view.backgroundColor = .white
-        self.navigationController?.isNavigationBarHidden = false
-        setupUI()
-        setupTermsOfServiceText()
-    }
+// 개인정보 처리방침 내용 레이블
+private let setupTermsOfServiceLabel: UILabel = {
+    let label = UILabel()
+    label.numberOfLines = 0 // 여러 줄 표시 가능하도록 설정
+    label.textAlignment = .left
+    label.font = UIFont.systemFont(ofSize: 14.0) // 폰트 크기 조절
+    label.textColor = .black // 텍스트 색상 설정
+    return label
+}()
 
-    private func setupUI() {
-        view.addSubview(termsOfServiceTextView)
-        NSLayoutConstraint.activate([
-            termsOfServiceTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            termsOfServiceTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            termsOfServiceTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            termsOfServiceTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-        ])
-    }
+private func setupUI() {
+    let scrollView = UIScrollView()
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(scrollView)
+    
+    scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+    scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+    
+    // 개인정보 처리방침 내용 레이블 추가
+    scrollView.addSubview(setupTermsOfServiceLabel)
+    
+    setupTermsOfServiceLabel.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+        setupTermsOfServiceLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 20),
+        setupTermsOfServiceLabel.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 20),
+        setupTermsOfServiceLabel.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor, constant: -20),
+        setupTermsOfServiceLabel.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -40) // ScrollView와 같은 너비로 설정하여 가로 스크롤이 나타나지 않도록 함
+    ])
+    
+    // 마지막 서브뷰의 하단을 ScrollView의 contentSize의 하단에 맞춰줌
+    NSLayoutConstraint.activate([
+        setupTermsOfServiceLabel.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -20)
+    ])
+}
 
-    private func setupTermsOfServiceText() {
-        let termsOfServiceText = """
-        [목적]\n
-        본 약관은 Pillanner 서비스(이하 '서비스')의 이용에 관한 조건 및 절차, 이용자와 서비스 제공자의 권리, 의무 및 책임사항 등 기본적인 사항을 규정함을 목적으로 합니다.\n
-        [서비스 이용]\n
-        서비스 이용자는 본 서비스를 개인적인 용도로만 이용할 수 있으며, 타인에게 임의로 양도하거나 상업적인 목적으로 이용할 수 없습니다.\n
-        [개인정보 수집 및 이용]\n
-        서비스 이용자가 서비스를 이용함에 있어 필요한 최소한의 개인정보는 수집될 수 있습니다.\n
-        [서비스 제공 중지 및 변경]\n
-        서비스 운영상 또는 기술상의 필요에 따라 서비스의 전부 또는 일부 제공이 중지될 수 있습니다.\n
-        [책임제한]\n
-        서비스 제공자는 서비스를 제공함에 있어 일반적으로 요구되는 주의와 선의를 기준으로 서비스를 제공합니다.\n
-        [분쟁해결]\n
-        서비스 이용자와 서비스 제공자 간에 발생한 분쟁에 대해는 우선적으로 합의를 이루어 해결하도록 합니다.\n
-        [약관 효력 및 변경]\n
-        본 약관은 서비스 제공자의 사전 공지 없이 변경될 수 있습니다.\n
-        """
-        
-        let attributedString = NSMutableAttributedString(string: termsOfServiceText)
-        
-        let boldAttributes: [NSAttributedString.Key: Any] = [
-            .font: FontLiteral.title2(style: .bold)
-        ]
-        let normalAttributes: [NSAttributedString.Key: Any] = [
-            .font: FontLiteral.body(style: .regular)
-        ]
-        
-        // "목적", "서비스 이용" 등의 부분을 굵게 만듦
-        let boldStrings = ["[목적]", "[서비스 이용]", "[개인정보 수집 및 이용]", "[서비스 제공 중지 및 변경]", "[책임제한]", "[분쟁해결]", "[약관 효력 및 변경]"]
-        boldStrings.forEach { boldString in
-            let range = (termsOfServiceText as NSString).range(of: boldString)
-            attributedString.addAttributes(boldAttributes, range: range)
-        }
-        
-        // 나머지 텍스트에 일반 스타일 적용
-        attributedString.addAttributes(normalAttributes, range: NSRange(location: 0, length: attributedString.length))
-        
-        termsOfServiceTextView.attributedText = attributedString
+
+private func setupTermsOfService() {
+    let privacyPolicyText = """
+  1. 목적
+
+본 약관은 Pillanner 서비스(이하 '서비스')의 이용에 관한 조건 및 절차, 이용자와 서비스 제공자의 권리, 의무 및 책임사항 등 기본적인 사항을 규정함을 목적으로 합니다.
+
+
+2. 서비스 이용
+
+서비스 이용자는 본 서비스를 개인적인 용도로만 이용할 수 있으며, 타인에게 임의로 양도하거나 상업적인 목적으로 이용할 수 없습니다.
+
+
+3. 개인정보 수집 및 이용
+서비스 이용자가 서비스를 이용함에 있어 필요한 최소한의 개인정보는 수집될 수 있습니다
+
+
+4. 서비스 제공 중지 및 변경
+
+서비스 운영상 또는 기술상의 필요에 따라 서비스의 전부 또는 일부 제공이 중지될 수 있습니다.
+
+
+5. 책임제한
+
+서비스 제공자는 서비스를 제공함에 있어 일반적으로 요구되는 주의와 선의를 기준으로 서비스를 제공합니다.
+
+
+6. 분쟁해결
+
+서비스 이용자와 서비스 제공자 간에 발생한 분쟁에 대해는 우선적으로 합의를 이루어 해결하도록 합니다.
+
+
+7. 약관 효력 및 변경
+
+본 약관은 서비스 제공자의 사전 공지 없이 변경될 수 있습니다.
+
+"""
+    
+    let attributedString = NSMutableAttributedString(string: privacyPolicyText)
+    let boldFontAttribute = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 16.0)]
+    
+    // 각 번호에 해당하는 부분의 범위를 찾아서 볼드 처리
+    let boldStrings = ["1. 목적","2. 서비스 이용", "3. 개인정보 수집 및 이용", "4. 서비스 제공 중지 및 변경", "5. 책임제한", "6. 분쟁해결", "7. 약관 효력 및 변경"]
+    for boldString in boldStrings {
+        let range = (privacyPolicyText as NSString).range(of: boldString)
+        attributedString.addAttributes(boldFontAttribute, range: range)
     }
+    
+    setupTermsOfServiceLabel.attributedText = attributedString
+}
 }
