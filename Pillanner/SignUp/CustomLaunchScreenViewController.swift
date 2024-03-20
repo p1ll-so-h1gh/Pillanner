8363cda323806a573715c9ac9f034290a39d303d
yktohub <rbgh2858@naver.com>
2024-03-20 19:42:23 +0900

    처음 실행하는 경우 에러핸들링 수정

--- Pillanner/SignUp/CustomLaunchScreenViewController.swift
+++ Pillanner/SignUp/CustomLaunchScreenViewController.swift
@@ -76,7 +76,12 @@ class CustomLaunchScreenViewController: UIViewController {
         switch(autoLoginActivate){
         case true :
             DataManager.shared.readUserData(userID: myID) { userData in
-                guard let userData = userData else { return }
+                guard let userData = userData else {
+                    let navVC = UINavigationController(rootViewController: LoginViewController())
+                    navVC.modalPresentationStyle = .fullScreen
+                    self.present(navVC, animated: true)
+                    return
+                }
                 if self.myID == userData["ID"] && self.myPW == userData["Password"] && userData["SignUpPath"] == "일반회원가입" {
                     Auth.auth().signIn(with: self.credential) { authData, error in
                         let currentUser = Auth.auth().currentUser
